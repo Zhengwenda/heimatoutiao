4934159053c9c097639da27c3336daf8c4b92f81
Zhengwenda <1263470162@qq.com>
2019-12-21 20:49:54 +0800

    axios拦截器

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
-import axios from 'axios'
+import './permission'
+import axios from './utils/request' // 引入自己封装的axios
 import 'element-ui/lib/theme-chalk/index.css'
 import '../src/styles/index.less'
 import components from './components/index.js'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+
 Vue.prototype.$axios = axios
 Vue.use(ElementUI)
 Vue.use(components)
